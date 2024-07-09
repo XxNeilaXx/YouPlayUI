d28b346eab681a2235125428097858bf63b9af98
Paulo Matos <38988316+PauloABM@users.noreply.github.com>
2024-07-08 21:41:41 -0300

    escolha de instrumento

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import '@/styles/globals.css';
 import { UserProvider } from '@auth0/nextjs-auth0/client';
 import { ThemeProvider } from '@/components/theme-provider';
 import { NavBar } from '@/components/nav-bar';
+import { Toaster } from '@/components/ui/toaster';
 
 export const metadata: Metadata = {
   title: 'XxNeilaXx',
@@ -40,6 +41,7 @@ export default function RootLayout({
             {children}
           </ThemeProvider>
         </UserProvider>
+        <Toaster />
       </body>
     </html>
   );
